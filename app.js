54e3cdef7d616d2d78198cf7167eeb6c163e797f
AshisMaity <maity.ashis651@gmail.com>
2023-03-14 12:18:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+
+app.listen(1998, async()=>{console.log('app is running on port 1998.....')});
+
+
+app.get('/', async(req, res)=>{
+    res.status(200).send({"message":"Hello Wolrd"})
+})
+
+app.get('/defame', async(req, res)=>{
+    res.status(200).send({"message":"Shame on you"})
+})
+
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,24 @@
+const  app = require('../app');
+const supertest = require('supertest')
+const { expect }= require('chai');
+
+
+const request = supertest(app);
+
+
+describe("POSITIVE TEST ",async()=>{
+    it('/ root', async()=>{
+        let res = await request.get('/');
+        expect(res.body).not.to.be.null;
+        expect(res.body.message).to.equal('Hello Wolrd');
+        expect(res.status).to.equal(200);
+    })
+
+    it('/defame', async()=>{
+        let res = await request.get('/defame');
+        
+        expect(res.status).to.equal(200)
+        expect(res.body).not.to.be.null;
+        expect(res.body.message).to.equal('Shame on you')
+    })
+})
